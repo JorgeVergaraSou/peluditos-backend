77d466ad273bc96ca979f8782a24b73cdce1a2e8
jorge vergara <jorgevergarasou@gmail.com>
2025-08-22 12:12:04 -0300

    cambios menores

--- src/pedidos/pedidos.controller.ts
+++ src/pedidos/pedidos.controller.ts
@@ -1,17 +1,15 @@
 // src/pedido/pedido.controller.ts
 
-import { Controller, Post, Body, Res, HttpStatus } from '@nestjs/common';
-
-import { Response } from 'express';
+import { Controller, Post, Body } from '@nestjs/common';
 import { PedidosService } from './pedidos.service';
 import { CrearPreferenciaDto } from './dto/crear-preferencia.dto';
 
 @Controller('pedidos')
 export class PedidosController {
-  constructor(private readonly pedidoService: PedidosService) {}
+  constructor(private readonly pedidoService: PedidosService) { }
 
-@Post('crear-preferencia')
-async crearPreferencia(@Body() dto: CrearPreferenciaDto) {
-  return this.pedidoService.crearPreferencia(dto);
-}
+  @Post('crear-preferencia')
+  async crearPreferencia(@Body() dto: CrearPreferenciaDto) {
+    return this.pedidoService.crearPreferencia(dto);
+  }
 }
